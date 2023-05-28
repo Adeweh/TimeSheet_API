3494f189569f4f4cf69497b1da015bcfd131eba8
Adeweh <dorcasabang@gmail.com>
2023-05-28 17:23:59 +0100

    updates

--- src/main/java/com/example/timesheet_api/repository/EmployeeRepository.java
+++ src/main/java/com/example/timesheet_api/repository/EmployeeRepository.java
@@ -1,4 +1,4 @@
 package com.example.timesheet_api.repository;
 
-public class EmployeeRepository {
+public interface EmployeeRepository {
 }
--- src/main/java/com/example/timesheet_api/repository/TimeRecordRepository.java
+++ src/main/java/com/example/timesheet_api/repository/TimeRecordRepository.java
@@ -1,4 +1,4 @@
 package com.example.timesheet_api.repository;
 
-public class TimeRecordRepository {
+public interface TimeRecordRepository {
 }
